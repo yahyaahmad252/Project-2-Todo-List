4c61fd6eae5830d7eb1f0da4052b1aeca3b4fedd
yahyaahmad252 <yahyaahmad252@icloud.com>
2021-12-21 20:52:53 +0300

    react stater code

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import CSS from './App.css'
+// import server from '../../backend/server'
+export default function App() {
+  const getData= ()=>{
+
+  }
+
+  return (
+    <div className='App'>
+      <p>app</p>
+      <button onClick={getData}>getTasks</button>
+    </div>
+  )
+}
